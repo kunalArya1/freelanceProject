9706a565bafac0a42c6ea659b107ac1c390bde3d
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2024-02-26 09:51:13 +0530

    Home Animation part completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import videobg from "./assets/HomePage.mp4";
-import { motion } from "framer-motion";
+import { FlatTree, motion } from "framer-motion";
 import leftvideo from "./assets/leftvideo.mp4";
 import rightvideo from "./assets/rightvido.mp4";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 const App = () => {
   const [leftbutton, setleftbutton] = useState(false);
@@ -10,33 +11,62 @@ const App = () => {
   return (
     <div className="h-screen w-full bg-gray-300 relative overflow-hidden">
       <div className=" h-full w-full bg-[rgba(0,0,0,.4)] absolute">
-        <img
-          className=" h-[8rem] absolute top-14 left-[47%] z-20"
-          src="https://pngimg.com/d/circle_PNG38.png"
-          alt=""
-        />
-
         {/** Left video part */}
+
+        {/********************************************************************************* */}
         <motion.div
           // onHoverStart={() => setleftbutton(true)}
           onMouseEnter={() => setleftbutton(true)}
-          className=" h-[15vh] w-[49vh] top-[45%] left-[10%] absolute z-10"
+          className=" h-[15vh] w-[51vh] top-[53%] left-[10%] absolute z-10"
         ></motion.div>
-
         {/** Left Text Part */}
-        <motion.div className=" overflow-hidden h-[20vh] w-[50vh] absolute top-[50%] left-[10%]">
-          <motion.div
-            onMouseLeave={() => setleftbutton(false)}
+        <motion.div
+          onMouseLeave={() => setleftbutton(false)}
+          initial={{ zIndex: leftbutton ? 20 : 0 }}
+          animate={{ zIndex: leftbutton ? 20 : 0 }}
+          transition={{ duration: 1 }}
+          className="h-[45%] w-[29%] absolute top-[38%] left-[10%]"
+        >
+          <motion.button
+            className="p-8 absolute  border-sky-400 w-[85%] left-0 border text-3xl uppercase font-semibold text-white"
             initial={{
-              top: leftbutton ? "0%" : "100%",
-              zIndex: leftbutton ? 10 : 0,
+              top: leftbutton ? "80%" : "33%",
+              width: leftbutton ? "100%" : "85%",
+              backgroundColor: leftbutton
+                ? "rgba(0,0,0,0.2)"
+                : "rgb(144, 205, 244)",
             }}
             animate={{
-              top: leftbutton ? "0%" : "100%",
-              zIndex: leftbutton ? 10 : 0,
+              top: leftbutton ? "80%" : "33%",
+              width: leftbutton ? "100%" : "85%",
+              backgroundColor: leftbutton
+                ? "rgba(0,0,0,0.2)"
+                : "rgb(144, 205, 244)",
             }}
-            // onHoverEnd={() => setleftbutton(false)}
-            className=" h-[20vh] w-[50vh] text-white absolute top-[100%] left-[0%] p-2 "
+            transition={{ duration: 0.4 }}
+          >
+            <div className=" flex items-center gap-6">
+              <p>Techanology</p>
+              <motion.span
+                initial={{ opacity: 0 }}
+                animate={{ opacity: leftbutton ? 1 : 0 }}
+                className=" inline-block"
+              >
+                <FaArrowRightLong />
+              </motion.span>
+            </div>
+          </motion.button>
+          <motion.div
+            initial={{
+              top: leftbutton ? "0%" : "80%",
+              opacity: leftbutton ? 1 : 0,
+            }}
+            animate={{
+              top: leftbutton ? "0%" : "80%",
+              opacity: leftbutton ? 1 : 0,
+            }}
+            // transition={{ duration: 0.4 }}
+            className=" absolute top[0%] text-white p-2"
           >
             <div className=" flex items-center">
               <div className=" h-1 w-16 bg-orange-500 mt-2"></div>
@@ -58,6 +88,7 @@ const App = () => {
         <motion.div
           initial={{ left: leftbutton ? "0%" : "-100%" }}
           animate={{ left: leftbutton ? "0%" : "-100%" }}
+          transition={{ duration: 0.4 }}
           className=" h-screen w-full bg-black absolute left-[-100%]"
         >
           <video className="w-full h-full object-cover" autoPlay muted loop>
@@ -65,26 +96,61 @@ const App = () => {
             Your browser does not support the video tag.
           </video>
         </motion.div>
-
         {/**Right Side Video */}
+        {/********************************************************************************* */}
         <motion.div
-          onHoverStart={() => setrightbutton(true)}
-          className=" h-[20vh] w-[50vh] top-[45%] right-[10%]  absolute z-10"
+          // onHoverStart={() => setleftbutton(true)}
+          onMouseEnter={() => setrightbutton(true)}
+          className=" h-[15vh] w-[51vh] top-[53%] right-[13.8%] absolute z-10"
         ></motion.div>
-
-        {/** Right text part */}
-        <motion.div className=" overflow-hidden h-[20vh] w-[50vh] absolute top-[50%] right-[9.1%]">
-          <motion.div
+        {/** Left Text Part */}
+        <motion.div
+          onMouseLeave={() => setrightbutton(false)}
+          initial={{ zIndex: rightbutton ? 20 : 0 }}
+          animate={{ zIndex: rightbutton ? 20 : 0 }}
+          transition={{ duration: 1 }}
+          className="h-[45%] w-[29%] absolute top-[38%] right-[10%]"
+        >
+          <motion.button
+            className="p-8 absolute border-sky-400 w-[85%] left-0 border text-3xl uppercase font-semibold text-white"
             initial={{
-              top: rightbutton ? "0%" : "100%",
-              zIndex: rightbutton ? 10 : 0,
+              top: rightbutton ? "80%" : "33%",
+              width: rightbutton ? "100%" : "85%",
+              backgroundColor: rightbutton
+                ? "rgba(0,0,0,0.2)"
+                : "rgb(144, 205, 244)",
             }}
             animate={{
-              top: rightbutton ? "0%" : "100%",
-              zIndex: rightbutton ? 10 : 0,
+              top: rightbutton ? "80%" : "33%",
+              width: rightbutton ? "100%" : "85%",
+              backgroundColor: rightbutton
+                ? "rgba(0,0,0,0.2)"
+                : "rgb(144, 205, 244)",
             }}
-            onHoverEnd={() => setrightbutton(false)}
-            className=" h-[20vh] w-[50vh] text-white absolute top-[100%] right-[0%] p-2 "
+            transition={{ duration: 0.4 }}
+          >
+            <div className=" flex items-center gap-6">
+              <p>Techanology</p>
+              <motion.span
+                initial={{ opacity: 0 }}
+                animate={{ opacity: rightbutton ? 1 : 0 }}
+                className=" inline-block"
+              >
+                <FaArrowRightLong />
+              </motion.span>
+            </div>
+          </motion.button>
+          <motion.div
+            initial={{
+              top: rightbutton ? "0%" : "80%",
+              opacity: rightbutton ? 1 : 0,
+            }}
+            animate={{
+              top: rightbutton ? "0%" : "80%",
+              opacity: rightbutton ? 1 : 0,
+            }}
+            // transition={{ duration: 0.4 }}
+            className=" absolute top[0%] text-white p-2"
           >
             <div className=" flex items-center">
               <div className=" h-1 w-16 bg-orange-500 mt-2"></div>
@@ -104,9 +170,10 @@ const App = () => {
         </motion.div>
 
         <motion.div
-          initial={{ right: rightbutton ? "0%" : "-100%" }}
-          animate={{ right: rightbutton ? "0%" : "-100%" }}
-          className=" h-screen w-full bg-black absolute right-[-100%]"
+          initial={{ left: rightbutton ? "0%" : "100%" }}
+          animate={{ left: rightbutton ? "0%" : "100%" }}
+          transition={{ duration: 0.4 }}
+          className=" h-screen w-full bg-black absolute left-[-100%]"
         >
           <video className="w-full h-full object-cover" autoPlay muted loop>
             <source src={rightvideo} type="video/mp4" />
@@ -114,28 +181,13 @@ const App = () => {
           </video>
         </motion.div>
       </div>
+
       {/** Main page Video  */}
+      {/********************************************************************************* */}
       <video className="w-full h-full object-cover" autoPlay muted loop>
         <source src={videobg} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-
-      {/** Left Button */}
-      <motion.button
-        className="absolute top-[50%] left-[10%] p-8 bg-sky-300 border-sky-400 border w-1/4 text-3xl uppercase font-semibold text-white"
-        initial={{ top: leftbutton ? "70%" : "50%" }}
-        animate={{ top: leftbutton ? "70%" : "50%" }}
-      >
-        Techanology
-      </motion.button>
-      {/* Right Button */}
-      <motion.button
-        initial={{ top: rightbutton ? "70%" : "50%" }}
-        animate={{ top: rightbutton ? "70%" : "50%" }}
-        className="absolute top-1/2 right-[10%] p-8 bg-sky-300 border-sky-400 border w-1/4 text-3xl uppercase font-semibold text-white"
-      >
-        Infrastructure
-      </motion.button>
     </div>
   );
 };
