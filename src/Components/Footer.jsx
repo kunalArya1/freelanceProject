40cb29d2d4ef2a05a990f4e4d9705e3b1b1fb572
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2024-03-03 23:43:41 +0530

    Footer Section Created

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,9 @@ const Footer = () => {
   return (
     <div className=" h-[70%] w-full bg-black">
       <div className=" h-[100%] w-full  bg-[rgba(32,157,218,1)] ">
-        // <p className=" font-bold text-center p-5">Footer Section</p>
+        <p className=" font-bold text-center p-5">Footer Section</p>
+        <p className=" font-bold text-center p-5">Footer Section</p>
+        <p className=" font-bold text-center p-5">Footer Section</p>
       </div>
     </div>
   );
