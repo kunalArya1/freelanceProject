266c6d003cbf079008ea37b4989ec0b5a1c06428
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2024-03-05 16:20:17 +0530

    Footer Section Changed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,8 +5,6 @@ const Footer = () => {
     <div className=" h-[70%] w-full bg-black">
       <div className=" h-[100%] w-full  bg-[rgba(32,157,218,1)] ">
         <p className=" font-bold text-center p-5">Footer Section</p>
-        <p className=" font-bold text-center p-5">Footer Section</p>
-        <p className=" font-bold text-center p-5">Footer Section</p>
       </div>
     </div>
   );
