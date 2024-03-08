aa2bf82148955cd9851b6eb2817937eb8b339de6
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2024-03-08 15:16:03 +0530

    Footer Section Responsive done

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className=" h-[70%] w-full bg-black">
-      <div className=" h-[100%] w-full  bg-[rgba(32,157,218,1)] ">
+    <div className=" h-[40%] md:h-[70%] lg:h-[70%] w-full bg-black">
+      <div className=" h-[100%] w-full  bg-[#82b2ca] ">
         <p className=" font-bold text-center p-5">Footer Section</p>
       </div>
     </div>
