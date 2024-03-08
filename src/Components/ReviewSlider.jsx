46b4af056edc80eefb6d392e245a0552a85ad40f
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2024-03-08 15:09:39 +0530

    Review Section MobileResponsive done

--- src/Components/ReviewSlider.jsx
+++ src/Components/ReviewSlider.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export const ReviewSlider = () => {
   return (
-    <div className=" h-[80%] w-full bg-black p-8">
-      <div className=" h-[90%] w-full bg-blue-300 m-auto">
+    <div className=" h-[60%] md:h-[90%] lg:h-[90%] w-full bg-black p-8 ">
+      <div className=" h-[99%] w-full  bg-blue-300 m-auto rounded-md">
         <p className=" text-center font-bold p-10">Review Section</p>
       </div>
     </div>
